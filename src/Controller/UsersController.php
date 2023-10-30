9440f6398aec13ec3ae8bc1373b33a5e33f590a5
kseb49 <kseb49@gmail.com>
2023-10-30 12:05:09 +0100

    delete an user is setted

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -5,11 +5,12 @@ namespace App\Controller;
 // use App\Entity\Clients;
 use App\Repository\UsersRepository;
 use App\Repository\ClientsRepository;
-use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('api')]
@@ -45,7 +46,7 @@ class UsersController extends AbstractController
         $client = $this->getUser();
         $user = $users->findOneBy(['id' => $id,'clients' => $client]);
         if ($user === null) {
-            return $this->json(["message" => "Vous n'avez pas de client avec cet identifiant"], 404);
+            return $this->json(["message" => "Vous n'avez pas d'utilisateurs avec cet identifiant"], 404);
         }
         return $this->json($user, context:['groups' => 'client_user']);
 
@@ -61,10 +62,19 @@ class UsersController extends AbstractController
 
 
     #[Route('/users/{id}', name:'delete_user', methods:'DELETE')]
-    #[IsGranted('ROLE_ADMIN', statusCode:403)]
-    public function deleteUser(int $id, UsersRepository $users)
+    #[IsGranted('ROLE_ADMIN', message:"Vous n'avez pas les droits suffisants pour effectuer cet action", statusCode:403)]
+    public function deleteUser(int $id, UsersRepository $users, EntityManagerInterface $entityManager) :JsonResponse | Response
     {
-        
+        $client = $this->getUser();
+        $user = $users->findOneBy(['id' => $id,'clients' => $client]);
+        if ($user === null) {
+            return $this->json(["message" => "Vous n'avez pas d'utilisateurs avec cet identifiant"], 404);
+        }
+
+        $entityManager->remove($user);
+        $entityManager->flush();
+        return new Response(status: 204);
+
     }
 
 
